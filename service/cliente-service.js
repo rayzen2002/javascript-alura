1129db55412a589af4710eda599afb7bfd34ce3c
Emanuel <emanuelzsmj@gmail.com>
2021-02-19 07:51:57 -0300

    Capitulo 1 : Comunicacao assincrona - Aula 2 : Criando template

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,8 +1,36 @@
+const criaNovaLinha = (nome , email) => {
+    const linhaNovoCliente = document.createElement('tr');
+    const conteudo = 
+    `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+    <ul class="tabela__botoes-controle">
+        <li><a href="../telas/edita_cliente.html?id=" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+    </ul>
+    </td>   
+    
+    `
+
+linhaNovoCliente.innerHTML = conteudo
+return linhaNovoCliente
+
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
+
+
+
+
 const http = new XMLHttpRequest()
 
 http.open('GET', 'http://localhost:3000/profile')
 http.send()
 http.onload = () => {
-    const data = http.response
-    console.log(data)
+    const data = JSON.parse(http.response)
+    data.forEach(element => {
+        tabela.appendChild(criaNovaLinha(element.nome,element.email))
+    });
 }
