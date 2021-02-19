91bcb1e2af10ad351ef6086148e5163fba128644
Emanuel <emanuelzsmj@gmail.com>
2021-02-19 17:51:01 -0300

    Capitulo 3 : Criar e remover clientes - aula 1 : Criando um cliente

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -4,7 +4,24 @@
         return resposta.json()
     })
 }
+
+    const criaCliente = (nome, email) => {
+        return fetch (`http://localhost:3000/profile`, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                nome: nome,
+                email: email
+            })
+
+            }).then( resposta => {
+                return resposta.body
+        })
+    }
    
    export const clienteService = {
-       listaClientes
+       listaClientes,
+       criaCliente
    }
